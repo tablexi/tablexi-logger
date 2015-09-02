1b1044649b6c4ee0e3b3fcebb9baf9bc731e506b
Bradley Schaefer <bradley.schaefer@gmail.com>
2015-09-02 17:08:15 -0400

    Make rubocop happy

--- lib/tablexi/logger/railtie.rb
+++ lib/tablexi/logger/railtie.rb
@@ -12,13 +12,13 @@ module Tablexi
         # we need to make sure the setup occurs before we check lograge.logger
         Lograge.setup(app)
 
-        if app.config.lograge.logger == Rails.logger
-          # We need to preserve lograge file format integrity if lograge is using
-          # Rails.logger - presumably error tracking services will report the errors
-          # so they aren't swallowed silently.
-          null_logger = ::Logger.new(File.open(File::NULL, "w"))
-          Tablexi.bare_logger = null_logger
-        end
+        return unless app.config.lograge.logger == Rails.logger
+
+        # We need to preserve lograge file format integrity if lograge is using
+        # Rails.logger - presumably error tracking services will report the errors
+        # so they aren't swallowed silently.
+        null_logger = ::Logger.new(File.open(File::NULL, "w"))
+        Tablexi.bare_logger = null_logger
       end
     end
   end
